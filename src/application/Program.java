93ad2df1b5014aea14fd6958b3e9768484f5e84d
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-26 11:58:23 -0300

    Update Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,16 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+import model.entities.Seller;
+
 public class Program {
     public static void main(String[] args) {
-        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("==== TEST 01: findById - Seller ===");
+        Seller seller = sellerDao.findById(3);
+        System.out.println(seller);
+        System.out.println("====================================");
     }
 }
