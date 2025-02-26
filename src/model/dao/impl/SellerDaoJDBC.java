8f53fcf492d77aad4db6e12ef0711f5f1eeea3b8
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-26 11:44:54 -0300

    Update SellerDaoJDBC.java

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -22,19 +22,16 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void insert(Seller obj) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void update(Seller obj) {
-        // TODO Auto-generated method stub
 
     }
 
     @Override
     public void deleteById(Integer id) {
-        // TODO Auto-generated method stub
 
     }
 
@@ -77,7 +74,6 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public List<Seller> findAll() {
-        // TODO Auto-generated method stub
         return null;
     }
 }
