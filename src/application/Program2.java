fe9d3fbef9a79f723cd41f5662a1c9feb95dfc94
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-26 15:19:58 -0300

    DepartmentDao insert

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,7 +1,16 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
     public static void main(String[] args) {
+        DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 
+        System.out.println("\n=== TEST 1: insert =======");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id: " + newDepartment.getId());
     }
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -1,9 +1,11 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-import java.sql.Connection;
+import java.sql.*;
 import java.util.List;
 
 public class DepartmentDaoJDBC implements DepartmentDao {
@@ -16,7 +18,36 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
     @Override
     public void insert(Department obj) {
+        PreparedStatement st = null;
+        try {
+            st = conn.prepareStatement(
+                    "INSERT INTO department "
+                      + "(Name) "
+                      + "VALUES "
+                      + "(?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+            st.setString(1, obj.getName());
 
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setId(id);
+                }
+                DB.closeResultSet(rs);
+            }
+            else {
+                throw new DbException("Unexpected error! No rows affected!");
+            }
+        }
+        catch (SQLException e) {
+            System.out.println("Unexpected error! " + e.getMessage());
+        }
+        finally {
+            DB.closeStatement(st);
+        }
     }
 
     @Override
