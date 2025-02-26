6c5ede4fa5147352274436f0095575c1586cc372
Luiz Fernando Santana Dal Pra <113379005+luuizfernando@users.noreply.github.com>
2025-02-26 16:03:29 -0300

    DepartmentDao delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -20,18 +20,19 @@ public class Program2 {
             System.out.println(dpt);
         }
 
+        System.out.println("\n=== TEST 3: insert =======");
+        Department newDepartment = new Department(null, "Music");
+        departmentDao.insert(newDepartment);
+        System.out.println("Inserted! New id: " + newDepartment.getId());
 
+        System.out.println("\n=== TEST 4: update =======");
+        Department dep2 = departmentDao.findById(1);
+        dep2.setName("Food");
+        departmentDao.update(dep2);
+        System.out.println("Update completed");
 
-//        System.out.println("\n=== TEST ?: insert =======");
-//        Department newDepartment = new Department(null, "Music");
-//        departmentDao.insert(newDepartment);
-//        System.out.println("Inserted! New id: " + newDepartment.getId());
-
-
-//        System.out.println("\n=== TEST ?: update =======");
-//        Department dep2 = departmentDao.findById(1);
-//        dep2.setName("Food");
-//        departmentDao.update(dep2);
-//        System.out.println("Update completed");
+        System.out.println("\n=== TEST 5: update =======");
+        departmentDao.deleteById(16);
+        System.out.println("Delete completed");
     }
 }
